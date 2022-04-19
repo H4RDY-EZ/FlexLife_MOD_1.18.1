cac9e47ecaf86128a3d3239bb00b76d88253a01a
H4RDY-EZ <DafuqPlaysGaming@gmail.com>
2022-04-19 23:28:35 +0400

    1) Added a pack of Sobranie 2) Added a translation to new item 3) Added new properties for Sobranie

--- src/main/java/com/finito/flexlife/item/ModItems.java
+++ src/main/java/com/finito/flexlife/item/ModItems.java
@@ -3,6 +3,7 @@ package com.finito.flexlife.item;
 import com.finito.flexlife.FlexLife;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,6 +14,9 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, FlexLife.MOD_ID);
 
     public static final RegistryObject<Item> SOBRANIE = ITEMS.register("sobranie",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(20)));
+
+    public static final RegistryObject<Item> SOBRANIE_PACK = ITEMS.register("a_pack_of_sobranie",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus){
