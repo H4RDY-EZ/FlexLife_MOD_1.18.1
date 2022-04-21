9a94fc44400df9de80cabf9996096922857127cf
H4RDY-EZ <DafuqPlaysGaming@gmail.com>
2022-04-21 20:16:12 +0400

    Added more items and translations to them

--- src/main/java/com/finito/flexlife/item/ModItems.java
+++ src/main/java/com/finito/flexlife/item/ModItems.java
@@ -18,6 +18,41 @@ public class ModItems {
 
     public static final RegistryObject<Item> SOBRANIE_PACK = ITEMS.register("a_pack_of_sobranie",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    // NEW ITEMS
+    // NEW ITEMS
+    // NEW ITEMS
+    // NEW ITEMS
+    // NEW ITEMS
+
+    public static final RegistryObject<Item> TOBACCO = ITEMS.register("tobacco",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> LIGHTER = ITEMS.register("lighter",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> PAPYRUS = ITEMS.register("papyrus",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> WINSTON = ITEMS.register("winston",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> CAPSULE = ITEMS.register("capsule",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> LD = ITEMS.register("ld",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> MARLBORO = ITEMS.register("marlboro",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> PRIMA = ITEMS.register("prima",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> GAS_SPRAY = ITEMS.register("gas_spray",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
