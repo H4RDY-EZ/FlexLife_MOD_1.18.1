fc571213983d665dfb04d52e6424dd52fd0501d1
FINITO <DafuqPlaysGaming@gmail.com>
2022-04-24 19:25:55 +0400

    Added new items. Added translations for new items. Added food.

---
+++ src/main/java/com/finito/flexlife/item/ModFoods.java
@@ -0,0 +1,24 @@
+package com.finito.flexlife.item;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+
+    public static final FoodProperties REDBIZON = (new FoodProperties.Builder()).nutrition(2).saturationMod(0.2F).alwaysEat().effect(new MobEffectInstance(MobEffects.BLINDNESS,600, 0), 1f).build();
+    public static final FoodProperties BLUEBIZON = (new FoodProperties.Builder()).alwaysEat().nutrition(2).saturationMod(0.2f).effect(new MobEffectInstance(MobEffects.REGENERATION, 600, 0), 1f).build();
+    public static final FoodProperties TWOAZNDONER = (new FoodProperties.Builder()).nutrition(6).saturationMod(0.6f).build();
+    public static final FoodProperties THREEAZNDONER = (new FoodProperties.Builder()).nutrition(6).saturationMod(0.8f).build();
+    public static final FoodProperties FOURAZNDONER = (new FoodProperties.Builder()).nutrition(8).saturationMod(0.8f).build();
+    public static final FoodProperties SHAURMANUM1 = (new FoodProperties.Builder()).nutrition(12).saturationMod(1f).build();
+    public static final FoodProperties AVTOSHDONER = (new FoodProperties.Builder()).nutrition(16).saturationMod(1.2f).build();
+    public static final FoodProperties KOVSERDONER = (new FoodProperties.Builder()).nutrition(10).saturationMod(0.5f).build();
+    public static final FoodProperties BASDIRMA = (new FoodProperties.Builder()).nutrition(10).saturationMod(1f).build();
+
+    public static final FoodProperties GOVODKA = (new FoodProperties.Builder()).nutrition(1).saturationMod(0f).effect(new MobEffectInstance(MobEffects.CONFUSION,300, 0),1f).build();
+
+    public static final FoodProperties JAGUAR = (new FoodProperties.Builder()).nutrition(2).saturationMod(0.1f).effect(new MobEffectInstance(MobEffects.CONFUSION,160, 0),1f).build();
+
+
+}
--- src/main/java/com/finito/flexlife/item/ModItems.java
+++ src/main/java/com/finito/flexlife/item/ModItems.java
@@ -45,12 +45,50 @@ public class ModItems {
     public static final RegistryObject<Item> GAS_SPRAY = ITEMS.register("gas_spray",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_ITEMS)));
 
+
+    public static final RegistryObject<Item> KULQABI = ITEMS.register("kulqabi",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_ITEMS)));
+
     // NEW ITEMS
     // NEW ITEMS
     // NEW ITEMS
 
-    public static final RegistryObject<Item> KULQABI = ITEMS.register("kulqabi",
+
+    public static final RegistryObject<Item> BAKI = ITEMS.register("baki",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_ITEMS)));
+
+    public static final RegistryObject<Item> REDBIZON = ITEMS.register("redbizon",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_FOODS).food(ModFoods.REDBIZON)));
+
+    public static final RegistryObject<Item> BLUEBIZON = ITEMS.register("bluebizon",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_FOODS).food(ModFoods.BLUEBIZON)));
+
+    public static final RegistryObject<Item> TWOAZNDONER = ITEMS.register("twoazndoner",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_FOODS).food(ModFoods.TWOAZNDONER)));
+
+    public static final RegistryObject<Item> THREEAZNDONER = ITEMS.register("threeazndoner",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_FOODS).food(ModFoods.THREEAZNDONER)));
+
+    public static final RegistryObject<Item> FOURAZNDONER = ITEMS.register("fourazndoner",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_FOODS).food(ModFoods.FOURAZNDONER)));
+
+    public static final RegistryObject<Item> SHAURMANUM1 = ITEMS.register("shaurmanum1",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_FOODS).food(ModFoods.SHAURMANUM1)));
+
+    public static final RegistryObject<Item> AVTOSHDONER = ITEMS.register("avtoshdoner",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_FOODS).food(ModFoods.AVTOSHDONER)));
+
+    public static final RegistryObject<Item> KOVSERDONER = ITEMS.register("kovserdoner",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_FOODS).food(ModFoods.KOVSERDONER)));
+
+    public static final RegistryObject<Item> BASDIRMA = ITEMS.register("basdirma",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_FOODS).food(ModFoods.BASDIRMA)));
+
+    public static final RegistryObject<Item> JAGUAR = ITEMS.register("jaguar",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_FOODS).food(ModFoods.JAGUAR)));
+
+    public static final RegistryObject<Item> GOVODKA = ITEMS.register("govodka",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FLEX_TAB_FOODS).food(ModFoods.GOVODKA)));
 
 
 
